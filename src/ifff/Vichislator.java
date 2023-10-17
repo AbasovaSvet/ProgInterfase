c68b7994040b2414d5d30021dbf2dcef5a3b5221
Svetlana <A.svet.mr@yandex.ru>
2023-10-17 17:03:08 +0300

    Initial commit

---
+++ src/ifff/Vichislator.java
@@ -0,0 +1,3 @@
+public interface Vichislator {
+    void asc( int a, int b, int rez);
+}
---
+++ src/oper/Plus.java
@@ -0,0 +1,7 @@
+public class Plus implements Vichislator{
+    @Override
+    public void asc(int a, int b, int rez) {
+
+    }
+
+}
